4f6449a76f1ec4a58d29249bb415da0cda3ef680
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-09-27 00:48:43 +0530

    Root Screen Created

---
+++ lib/quiz.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    super.initState();
+    activeScreen = StartScreen(switchScreen);
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 128, 39, 176),
+        body: Container(
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
