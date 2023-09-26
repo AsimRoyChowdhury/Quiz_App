537c9c75bfd6950da964759737237dc83b3f983b
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-09-27 00:49:26 +0530

    main file updated with root screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 128, 39, 176),
-        body: Container(
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(Quiz());
 }
