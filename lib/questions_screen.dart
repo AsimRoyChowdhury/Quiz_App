5f49d90dd3ff51dc1ca58e614a9a2cceff50248a
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-09-27 22:07:54 +0530

    Updated Questions Screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen(this.onSelectAnswer, {super.key});
+
+  final void Function(String answer) onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() {
@@ -10,8 +15,44 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreen extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion(String answer) {
+    widget.onSelectAnswer(answer);
+    setState(() {
+      currentQuestionIndex != 4
+          ? currentQuestionIndex++
+          : currentQuestionIndex = 0;
+    });
+  }
+
   @override
   Widget build(context) {
-    return const Center(child: const Text("Questions"));
+    final currentQuestion = questions[currentQuestionIndex];
+
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                textAlign: TextAlign.center,
+                currentQuestion.text,
+                style: GoogleFonts.mooli(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 40),
+              ...currentQuestion.getSuffledAnswers().map((answerText) {
+                return AnswerButton(answerText, () {
+                  answerQuestion(answerText);
+                });
+              })
+            ]),
+      ),
+    );
   }
 }
