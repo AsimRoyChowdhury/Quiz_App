c61c6b9c321839f0ea05f73f69bf376ee55fd6e1
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-09-27 22:08:56 +0530

    Added shuffledanswerlist function

--- lib/models/quiz_questions.dart
+++ lib/models/quiz_questions.dart
@@ -1,6 +1,12 @@
-class QuizQuestions{
+class QuizQuestions {
   const QuizQuestions(this.text, this.answers);
 
   final String text;
   final List<String> answers;
+
+  List<String> getSuffledAnswers() {
+    final List<String> shuffledAnswers = List.of(answers);
+    shuffledAnswers.shuffle();
+    return shuffledAnswers;
+  }
 }
